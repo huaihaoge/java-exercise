fc170afc3f5b278cda66e77dbb7ac55ad862f067
陈浩chh <haochh.chen@qunar.com>
2020-04-14 01:33:26 +0800

    day2

---
+++ src/main/java/com/haochh/chen/exerise/day2/TreeNode.java
@@ -0,0 +1,16 @@
+package com.haochh.chen.exerise.day2;
+
+/**
+ * Created by admin on 2020/4/14.
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
